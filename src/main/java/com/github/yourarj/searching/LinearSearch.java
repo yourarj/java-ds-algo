748504a719c2cbc2e4305b8e30f5c7f13b61244b
yourarj <1359226+yourarj@users.noreply.github.com>
2023-09-10 18:26:48 +0530

    Linear Search Implementation
    Signed-off-by: yourarj <1359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Sep 10 18:26:48 2023 +0530
    
     On branch master
     Changes to be committed:
    	modified:   .idea/misc.xml
    	modified:   pom.xml
    	modified:   src/main/java/com/github/yourarj/searching/LinearSearch.java
    	new file:   src/test/java/com/github/yourarj/searching/LinearSearchTest.java

--- src/main/java/com/github/yourarj/searching/LinearSearch.java
+++ src/main/java/com/github/yourarj/searching/LinearSearch.java
@@ -1,4 +1,14 @@
 package com.github.yourarj.searching;
 
-public class LinearSearch {
+/**
+ * Implements Linear search
+ * O(n)
+ */
+public class LinearSearch<T> {
+    public int linearSearch( T needle, T[] hayStack) {
+        for(int i = 0; i< hayStack.length; i++){
+            if (needle.equals(hayStack[i])) return i;
+        }
+        return -1;
+    }
 }
