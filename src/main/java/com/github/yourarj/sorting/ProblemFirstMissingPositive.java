7b256137413fd4e46bc33f88b8f8465496c555fa
yourarj <1359226+yourarj@users.noreply.github.com>
2023-09-18 22:34:45 +0530

    Added first missing positive problem statement
    todo: solution
    
    Signed-off-by: yourarj <1359226+yourarj@users.noreply.github.com>
    
     Date:      Mon Sep 18 22:34:45 2023 +0530
    
     On branch master
    
     Changes to be committed:
    	new file:   src/main/java/com/github/yourarj/sorting/ProblemFirstMissingPositive.java
    	new file:   src/main/java/com/github/yourarj/sorting/ProblemFirstMissingPositive.md
    	new file:   src/test/java/com/github/yourarj/sorting/ProblemFirstMissingPositiveTest.java

---
+++ src/main/java/com/github/yourarj/sorting/ProblemFirstMissingPositive.java
@@ -0,0 +1,9 @@
+package com.github.yourarj.sorting;
+
+public class ProblemFirstMissingPositive {
+  public int firstMissingPositive(int[] nums) {
+
+    // invalid value
+    return 0;
+  }
+}
---
+++ src/test/java/com/github/yourarj/sorting/ProblemFirstMissingPositiveTest.java
@@ -0,0 +1,34 @@
+package com.github.yourarj.sorting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProblemFirstMissingPositiveTest {
+  private final ProblemFirstMissingPositive target = new ProblemFirstMissingPositive();
+
+  @Test
+  void firstMissingPositiveCase01() {
+    int[] input = {1, 2, 0};
+    int expectedResult = 3;
+    int actualResult = target.firstMissingPositive(input);
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  void firstMissingPositiveCase02() {
+    int[] input = {3, 4, -1, 1};
+    int expectedResult = 2;
+    int actualResult = target.firstMissingPositive(input);
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  void firstMissingPositiveCase03() {
+    int[] input = {7, 8, 9, 11, 12};
+    int expectedResult = 1;
+    int actualResult = target.firstMissingPositive(input);
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+}
