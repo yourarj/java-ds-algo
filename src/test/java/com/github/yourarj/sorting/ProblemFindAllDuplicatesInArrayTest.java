7ed5984b6a5c4a570bd544d58c633a1c33331d54
yourarj <11359226+yourarj@users.noreply.github.com>
2023-09-19 05:47:16 +0530

    fixes to email and code
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Tue Sep 19 05:47:16 2023 +0530
    
     On branch master
    
     Changes to be committed:
    	modified:   src/test/java/com/github/yourarj/sorting/ProblemFindAllDuplicatesInArrayTest.java
    	modified:   src/test/java/com/github/yourarj/sorting/ProblemFirstMissingPositiveTest.java

--- src/test/java/com/github/yourarj/sorting/ProblemFindAllDuplicatesInArrayTest.java
+++ src/test/java/com/github/yourarj/sorting/ProblemFindAllDuplicatesInArrayTest.java
@@ -1,6 +1,5 @@
 package com.github.yourarj.sorting;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
--- src/test/java/com/github/yourarj/sorting/ProblemFirstMissingPositiveTest.java
+++ src/test/java/com/github/yourarj/sorting/ProblemFirstMissingPositiveTest.java
@@ -1,6 +1,5 @@
 package com.github.yourarj.sorting;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
