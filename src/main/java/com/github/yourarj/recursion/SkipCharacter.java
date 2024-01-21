850229f75ff352dc62840e99fd2de683fbf16032
yourarj <11359226+yourarj@users.noreply.github.com>
2024-01-21 15:37:22 +0530

    recursion: Skip a character
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Jan 21 15:37:22 2024 +0530
    
     On branch master
    
     Changes to be committed:
    	new file:   src/main/java/com/github/yourarj/recursion/SkipCharacter.java
    	new file:   src/test/java/com/github/yourarj/recursion/SkipCharacterTest.java

---
+++ src/main/java/com/github/yourarj/recursion/SkipCharacter.java
@@ -0,0 +1,2 @@
+package com.github.yourarj.recursion;public class SkipCharacter {
+}
---
+++ src/test/java/com/github/yourarj/recursion/SkipCharacterTest.java
@@ -0,0 +1,10 @@
+package com.github.yourarj.recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SkipCharacterTest {
+
+  @Test
+  void skipACharacter() {}
+}
