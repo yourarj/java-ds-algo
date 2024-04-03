19f3992254f765bf9a685d0726bce96321afd567
yourarj <11359226+yourarj@users.noreply.github.com>
2024-04-03 07:43:37 +0530

    Balanced Binary Tree
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Wed Apr 3 07:43:37 2024 +0530
    
     On branch graph-balanced-binary-tree
     Changes to be committed:
    	new file:   src/main/java/com/github/yourarj/graph/BalancedBinaryTree.java
    	new file:   src/main/java/com/github/yourarj/graph/BinaryTreeNode.java
    	new file:   src/test/java/com/github/yourarj/graph/BalancedBinaryTreeTest.java

---
+++ src/main/java/com/github/yourarj/graph/BinaryTreeNode.java
@@ -0,0 +1,20 @@
+package com.github.yourarj.graph;
+
+public class BinaryTreeNode {
+
+  int val;
+  BinaryTreeNode left;
+  BinaryTreeNode right;
+
+  BinaryTreeNode() {}
+
+  BinaryTreeNode(int val) {
+    this.val = val;
+  }
+
+  BinaryTreeNode(int val, BinaryTreeNode left, BinaryTreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
