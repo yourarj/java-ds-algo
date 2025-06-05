1400a0292e4c991a575106ca3a5380b36bf2c510
yourarj <11359226+yourarj@users.noreply.github.com>
2025-06-05 22:34:52 +0530

    Solution: Smallest window containing all chars
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Thu Jun 5 22:34:52 2025 +0530
    
     On branch geeksforgeeks-amzn-sde-dsa
    
     Changes to be committed:
    	new file:   src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/smallest_window_containing_all_chars/Solution.java
    	new file:   src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/smallest_window_containing_all_chars/SolutionTest.java

---
+++ src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/smallest_window_containing_all_chars/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.github.yourarj.gfg_amzn_sde_dsa.smallest_window_containing_all_chars;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  void smallestWindow_Test_01() {
+    final String s = "timetopractice", p = "toc";
+    final String expected = "toprac";
+    final String actual = Solution.smallestWindow(s, p);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void smallestWindow_Test_02() {
+    final String s = "zoomlazapzo", p = "oza";
+    final String expected = "apzo";
+    final String actual = Solution.smallestWindow(s, p);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  void smallestWindow_Test_03() {
+    final String s = "zoomaazbaaacapzoabc", p = "abc";
+    final String expected = "abc";
+    final String actual = Solution.smallestWindow(s, p);
+    Assertions.assertEquals(expected, actual);
+  }
+}
