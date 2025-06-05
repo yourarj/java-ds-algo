4c6d399aaf72b5cd051f04d15f01814f86f5de1a
yourarj <11359226+yourarj@users.noreply.github.com>
2025-06-06 01:01:39 +0530

    Solution: linkedlist remove cycle
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Jun 6 01:01:39 2025 +0530
    
     On branch geeksforgeeks-amzn-sde-dsa
    
     Changes to be committed:
    	modified:   src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/Solution.java
    	modified:   src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/SolutionTest.java

--- src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/SolutionTest.java
+++ src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/SolutionTest.java
@@ -1,4 +1,38 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.github.yourarj.gfg_amzn_sde_dsa.linkedlist.removecycle;
+
+import com.github.yourarj.gfg_amzn_sde_dsa.linkedlist.common.Node;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class SolutionTest {
 
+  @Test
+  void removeCycle_Test_01() {
+    final Node<Integer> node = new Node<Integer>(new Integer[]{1, 2, 3, 4, 5});
+    node.addCycle(3);
+    final Node<Integer> expected = new Node<Integer>(new Integer[]{1, 2, 3, 4, 5});
+    final Node<Integer> actual = Solution.removeCycle(node);
+
+    Assertions.assertIterableEquals(expected, actual);
+  }
+
+  @Test
+  void removeCycle_Test_02() {
+    final Node<Integer> node = new Node<Integer>(new Integer[]{1, 2, 3, 4, 5});
+    node.addCycle(1);
+    final Node<Integer> expected = new Node<Integer>(new Integer[]{1, 2, 3, 4, 5});
+    final Node<Integer> actual = Solution.removeCycle(node);
+
+    Assertions.assertIterableEquals(expected, actual);
+  }
+
+  @Test
+  void removeCycle_Test_03() {
+    final Node<Integer> node = new Node<Integer>(new Integer[]{1});
+    node.addCycle(1);
+    final Node<Integer> expected = new Node<Integer>(new Integer[]{1});
+    final Node<Integer> actual = Solution.removeCycle(node);
+
+    Assertions.assertIterableEquals(expected, actual);
+  }
 }
