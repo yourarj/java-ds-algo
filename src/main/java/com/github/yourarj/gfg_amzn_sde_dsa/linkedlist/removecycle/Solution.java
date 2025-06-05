48aba1d53cf3e558e939e871c93ed6b25f186568
yourarj <11359226+yourarj@users.noreply.github.com>
2025-06-06 01:01:39 +0530

    Solution: linkedlist remove cycle
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Fri Jun 6 01:01:39 2025 +0530
    
     On branch geeksforgeeks-amzn-sde-dsa
    
     Changes to be committed:
    	modified:   src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/Solution.java
    	modified:   src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/SolutionTest.java

--- src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/Solution.java
+++ src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/linkedlist/removecycle/Solution.java
@@ -1,4 +1,38 @@
 package com.github.yourarj.gfg_amzn_sde_dsa.linkedlist.removecycle;
 
+
+import com.github.yourarj.gfg_amzn_sde_dsa.linkedlist.common.Node;
+
 public class Solution {
+  public static Node<Integer> removeCycle(final Node<Integer> list) {
+    if (list.next == null) return list;
+
+    Node<Integer> slow = list, fast = list;
+
+    slow = slow.next;
+    fast = fast.next.next;
+
+    while (null != fast && null != fast.next) {
+      if (slow == fast) break;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    if (slow == fast) {
+      slow = list;
+      if (slow != fast) {
+        while (slow.next != fast.next) {
+          slow = slow.next;
+          fast = fast.next;
+        }
+      } else {
+        while (fast.next != slow) {
+          fast = fast.next;
+        }
+      }
+      fast.next = null;
+    }
+
+    return list;
+  }
 }
