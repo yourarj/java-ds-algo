e506422ac6710ae7696debcee49ecd145cc5df8d
yourarj <11359226+yourarj@users.noreply.github.com>
2025-06-15 17:20:24 +0530

    Solution: Merge Sort
    Signed-off-by: yourarj <11359226+yourarj@users.noreply.github.com>
    
     Date:      Sun Jun 15 17:20:24 2025 +0530
    
     On branch geeksforgeeks-amzn-sde-dsa
    
     Changes to be committed:
    	new file:   src/main/java/com/github/yourarj/gfg_amzn_sde_dsa/sorting/merge_sort/Solution.java
    	new file:   src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/sorting/merge_sort/SolutionTest.java

---
+++ src/test/java/com/github/yourarj/gfg_amzn_sde_dsa/sorting/merge_sort/SolutionTest.java
@@ -0,0 +1,46 @@
+package com.github.yourarj.gfg_amzn_sde_dsa.sorting.merge_sort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+  @Test
+  void sort012Test_01() {
+    final int[] input = {0, 1, 2, 0, 1, 2};
+    final int[] output = {0, 0, 1, 1, 2, 2};
+    Solution.sort(input);
+    Assertions.assertArrayEquals(input, output);
+  }
+
+  @Test
+  void sort012Test_02() {
+    final int[] input = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+    final int[] output = {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2};
+    Solution.sort(input);
+    Assertions.assertArrayEquals(input, output);
+  }
+
+  @Test
+  void sort012Test_03() {
+    final int[] input = {0, 1, 2};
+    final int[] output = {0, 1, 2};
+    Solution.sort(input);
+    Assertions.assertArrayEquals(input, output);
+  }
+
+  @Test
+  void sort012Test_04() {
+    final int[] input = {1, 0};
+    final int[] output = {0, 1};
+    Solution.sort(input);
+    Assertions.assertArrayEquals(input, output);
+  }
+
+  @Test
+  void sort012Test_05() {
+    final int[] input = {0};
+    final int[] output = {0};
+    Solution.sort(input);
+    Assertions.assertArrayEquals(input, output);
+  }
+}
